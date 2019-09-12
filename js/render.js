f5f37c0a56bc62335c0cef1ea2d9333befe654a4
digitalmoleman <digitalmoleguy@gmail.com>
2019-09-12 14:42:19 +0200

    mobile dialogue fix

--- js/render.js
+++ js/render.js
@@ -659,7 +659,7 @@ class DialogueHandler {
     draw() {
         render.rect(block(3), canvasHeight - block(4.5), canvasWidth - block(6), block(4), "#00408080", 0); // background
         render.text(this.currentTextBox.speakerName, block(3), block(13.5), 12, "#fff", 0);
-        render.text(this.displayedText, block(4), block(14.5), 16, "#fff");
+        render.text(this.displayedText, block(4), canvasHeight - block(3.5), 16, "#fff");
         if (this.currentTextBox.text.length == this.textProg) render.text(">", block(28 + (Math.sin(gameClock / 5) / 4)), block(16.5), 32)
     }
 }
